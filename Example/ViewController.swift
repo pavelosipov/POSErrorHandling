9d0cceed4a550c5e52b8d1a6740f11c1ef513867
Dmitry Golubev <dmitry.golubev@vk.team>
2023-10-02 19:06:19 +0300

    добавление схем

---
+++ Example/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Example
+//
+//  Created by dmitry.golubev on 02.10.2023.
+//  Copyright © 2023 Pavel Osipov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
